e3b0f62517df37837a6da17a18ba4af42db949ee
donut <donut@deb.dpy>
2022-12-22 15:06:40 -0600

    messy code here

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const fields = Array.from(document.getElementsByTagName("fieldset"));
+const form = document.querySelector("form");
 // console.log(fields);
 const steps = Array.from(document.querySelectorAll(".step"));
 const labels = Array.from(document.querySelectorAll("[data-yearly]"));
@@ -6,14 +7,44 @@ const cb = Array.from(document.querySelectorAll(".cb"));
 const next = document.querySelector(".next");
 const previous = document.querySelector(".previous");
 const charge = document.querySelector(".charge");
+const change = document.querySelector(".change");
+charge.checked = false;
 const summary = document.createElement("span");
 summary.classList.add("total-cost");
 // console.log(labels);
+let removeSteps = () => {
+  steps.forEach((step) => {
+    step.classList.remove("light-up");
+  });
+};
+let checkFields = (field) => {
+  let inputs = Array.from(field.querySelectorAll("[required]"));
+
+  console.log(inputs);
+  for (let i = 0; i < inputs.length; i++) {
+    let validation = inputs[i].parentElement.querySelector(".validation");
+    inputs[i].classList.remove("fail");
+    validation.classList.remove("fail");
+    if (inputs[i].matches(":invalid")) {
+      inputs[i].classList.add("fail");
+      validation.classList.add("fail");
+      return false;
+    }
+  }
+  return true;
+};
+let changeToConfirm = (element) => {
+  if (element.nextElementSibling.matches(".last")) {
+    next.textContent = "Confirm";
+  } else next.textContent = "Next step";
+};
 let translate = (e) => {
+  next.value = "Next step";
+  removeSteps();
   for (i = 0; i < fields.length; i++) {
-    steps.forEach((step) => {
-      step.classList.remove("light-up");
-    });
+    if (checkFields(fields[i]) === false) {
+      return;
+    }
     if (fields[i].dataset.current !== undefined) {
       if (
         fields[i + 1] !== undefined &&
@@ -22,6 +53,7 @@ let translate = (e) => {
       ) {
         console.log("delete next", fields[i]);
         next.classList.add("disable");
+        previous.classList.add("disable");
       } else {
         next.classList.remove("disable");
       }
@@ -36,12 +68,19 @@ let translate = (e) => {
       }
       if (e.target === next) {
         let nextField = fields[i].nextElementSibling;
-        if (nextField === null) {
-          nextField = fields[0];
+        if (nextField.matches(".last")) {
+          previous.classList.add("disable");
+        }
+        if (nextField.matches(".summary")) {
+          countPricing();
         }
         delete fields[i].dataset.current;
         nextField.dataset.current = "";
-        steps[i + 1].classList.toggle("light-up");
+        if (steps[i + 1] !== undefined) {
+          steps[i + 1].classList.toggle("light-up");
+        }
+
+        //
       } else {
         console.log("else");
         let previousField = fields[i].previousElementSibling;
@@ -62,18 +101,73 @@ let translate = (e) => {
   //      else return;
   //   });
 };
-
-let pricing = () => {
+let removeChildren = (parent) => {
+  let toRemove = Array.from(document.querySelectorAll(".may-remove"));
+  toRemove.forEach((remove) => {
+    parent.removeChild(remove);
+  });
+};
+let insertChoices = (total) => {
+  let toInsert = document.querySelector(".sum-up");
+  let mainPlan = document.querySelector(".main-plan");
+  let mainPrice = mainPlan.nextElementSibling;
+  mainPlan.textContent = "";
+  mainPrice.textContent = "";
+  let planField = document.querySelector(".plan");
+  let addonsField = document.querySelector(".addons");
+  //
+  let addonsAll = Array.from(
+    addonsField.querySelectorAll("input:not(.identify):checked")
+  );
+  console.log(addonsAll);
+  let planFieldOption = planField.querySelector("input:checked");
+  //
+  //
+  let optionName = planFieldOption.parentElement.childNodes[0].textContent;
+  mainPlan.textContent = optionName;
+  console.log(total);
+  if (charge.checked) {
+    summary.textContent = `+$${total.sumYearly}/yr`;
+    mainPlan.textContent += " (Yearly)";
+    mainPrice.textContent = planFieldOption.parentElement.dataset.yearly;
+  } else {
+    summary.textContent = `+$${total.sumMonthly}/mo`;
+    mainPlan.textContent += " (Monthly)";
+    mainPrice.textContent = planFieldOption.parentElement.dataset.monthly;
+  }
+  removeChildren(toInsert);
+  addonsAll.forEach((a) => {
+    console.log(a);
+    let order = document.createElement("div");
+    order.classList.add("order");
+    order.classList.add("may-remove");
+    order.dataset.grid = "";
+    let choice = document.createElement("span");
+    choice.classList.add("choice");
+    let price = document.createElement("span");
+    price.classList.add("price");
+    let parent = a.parentElement;
+    choice.textContent = parent.childNodes[0].textContent;
+    price.textContent = parent.dataset.monthly;
+    if (charge.checked) {
+      price.textContent = parent.dataset.yearly;
+    }
+    order.appendChild(choice);
+    order.appendChild(price);
+    toInsert.appendChild(order);
+  });
+  next.value = "Confirm";
+  //
+};
+let countPricing = () => {
   let total = {
     sumMonthly: 0,
     sumYearly: 0,
   };
   let planField = document.querySelector(".plan");
   let addonsField = document.querySelector(".addons");
-  let planFieldOption = planField.querySelector('[type="radio"]:checked');
-  console.log(planFieldOption);
+  let planFieldOption = planField.querySelector("input:checked");
   let planLabel = planFieldOption.parentElement;
-  console.log(planLabel);
   let planPricing = {
     monthly: parseFloat(planLabel.dataset.monthly.split("/")[0].slice(1)),
     yearly: parseFloat(planLabel.dataset.yearly.split("/")[0].slice(1)),
@@ -85,7 +179,6 @@ let pricing = () => {
   console.log(addons);
   addons.forEach((addon) => {
     let addonParent = addon.parentElement;
-    console.log(addonParent);
     let monthly = parseFloat(
       addonParent.dataset.monthly.split("/")[0].slice(2)
     );
@@ -94,12 +187,7 @@ let pricing = () => {
     total.sumMonthly += monthly;
     total.sumYearly += yearly;
   });
-  console.log(total);
-  if (charge.checked) {
-    summary.textContent = `+$${total.sumYearly}/yr`;
-  } else {
-    summary.textContent = `+$${total.sumMonthly}/mo`;
-  }
+  insertChoices(total);
 };
 
 next.addEventListener("click", translate);
@@ -110,15 +198,25 @@ charge.addEventListener("click", () => {
   });
 });
 cb.forEach((c) => {
-  c.nextElementSibling.style.appearance = "none";
+  c.checked = false;
+  c.nextElementSibling.checked = false;
   c.addEventListener("click", () => {
     console.log(c.checked);
     console.log(c.nextElementSibling);
     if (c.checked) {
-      c.nextElementSibling.style.appearance = "checkbox";
+      c.nextElementSibling.checked = true;
     } else {
-      c.nextElementSibling.style.appearance = "none";
+      c.nextElementSibling.checked = false;
     }
   });
+});
+change.addEventListener("click", (e) => {
+  e.preventDefault();
+  removeSteps();
+  steps[1].classList.toggle("light-up");
+  let current = document.querySelector("[data-current]");
+  let option = document.querySelector(".plan");
+  delete current.dataset.current;
+  option.dataset.current = "";
 });
 document.querySelector(".total").appendChild(summary);
